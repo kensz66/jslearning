037e10449cb0099d721db936316d0995aa54c3ef
kensz66 <kensz66@126.com>
2019-03-17 05:21:34 +0800

    @

--- 使用过的js函数.js
+++ 使用过的js函数.js
@@ -101,7 +101,7 @@ typeof null; // returns "object"
 
 
 
-  //>>>>>>>>>>>>>>>>>instanceof 判断对象的类型(如 Date, Array, Number, Math等等...)
+  //>>>>>>>>>>>>>>>>>instanceof 判断实例对象的类型(如 Date, Array, Number, Math等等...)
   // 注意区别数据类型和对象类型
   var theDay = new Date(1995, 12, 17);
   if (theDay instanceof Date) {
@@ -112,4 +112,71 @@ typeof null; // returns "object"
 // this在一个方法中,一般是代表正在调用该方法的那个对象(哪个调用了this所在位置的方法,哪个就是this代表的对象).
 
 
-// >>>>>>>>>>>>>>>>>>>>>>>>>>   super左值表达式  >>>>>>>>>>>>>>>>>>
+//===================   ...扩展语句 =====================
+var a = [1, 2, 3];
+var b = [...a, 4, 5, 6];
+console.log(b);
+
+
+/* 附： 如何使用ES6的新特性， 又能保证浏览器的兼容？
+
+针对 ES6 的兼容性问题， 很多团队为此开发出了多种语法解析转换工具， 把我们写的 ES6 语法转换成 ES5， 相当于在 ES6 和浏览器之间做了一个翻译官。 比较通用的工具方案有 babel， jsx， traceur， es6 - shim 等。 */
+
+
+// =============  String字符串对象的方法和属性  =================
+var a = 'abcde';
+var b = a.length; // 返回 b=5
+var i = a[2]; //返回 i='c',通过此方法可以检索特定字符串
+// idexof用于在字符串中查找子字符串, 如果找不到, 则返回值 -1;
+if (browserType.indexOf('xyz') !== -1) {
+  // do stuff with the string
+}
+a.slice(0, 3); //返回 'abc'
+a.slice(2, 4); //返回 'cd'
+//这里第一参数为索引号,第二个参数为第几个位置.所以得到的字符串的长度就是 第二个参数减去第一参数
+//直观的说,就是从第一个索引号(2)的位置开始,往后数(4-2=2)个字符(cd).
+
+// 此外， 如果您知道要在某个字符之后提取字符串中的所有剩余字符， 则不必包含第二个参数， 而只需要包含要从中提取的字符位置 字符串中的其余字符。 尝试以下：
+var browserType = 'mozalla';
+browserType.slice(2);  //返回 'zalla'
+// 这返回“ zilla” - 这是因为2的字符位置是字母z， 并且因为没有包含第二个参数， 所以返回的子字符串是字符串中的所有剩余字符。
+
+// 字符转换大小写: string.toLowerCase();
+//替换字符串的某个部分: browserType.replace('moz','van'); 返回 vanzilla
+//注意,这需要赋值给一个新变量, 因为这个操作不会直接更改原字符串变量的值,只会得到一个新的字符串.如果要改变原字符串,将这个新字符串赋值给原字符串的变量就可.
+
+
+// =================数组与字符串的相互转换===============
+
+/* .split('参数') 分割字符串成数组, 参数为: 分隔符
+var myData = 'Manchester,London,Liverpool,Birmingham,Leeds,Carlisle';
+var myArray = myData.split(',');
+返回数组[Manchester, London, Liverpool, Birmingham, Leeds, Carlisle] */
+
+/* .join('连接数组各个值的符号'): 将数组连接成字符串
+var dogNames = ["Rocket", "Flash", "Bella", "Slugger"];
+dogNames.join(','); //返回字符串  "Rocket, Flash, Bella, Slugger"
+dogNames.join();
+返回字符串 "RocketFlashBellaSlugger" */
+
+// 不要用toString()去转数组为字符串,因为会加上数组分割的逗号,不稳定.
+
+
+//================ 添加和删除数组项 ========================
+
+/* .push('') 方法将一个或多个元素添加到数组的末尾， 并返回该数组的新长度。
+var sports = ["soccer", "baseball"];
+var total = sports.push("football", "swimming");
+console.log(sports);
+// ["soccer", "baseball", "football", "swimming"]
+console.log(total);
+// 4
+ */
+
+/* .pop() 方法用于从一个数组中删除并返回最后一个元素。
+let myFish = ["angel", "clown", "mandarin", "surgeon"];
+let popped = myFish.pop();
+console.log(myFish);
+// ["angel", "clown", "mandarin"]
+console.log(popped);
+// surgeon */
