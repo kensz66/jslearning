37acc5c0bcebb04fc0bc09d1a1fa465cfe858632
kensz66 <kensz66@126.com>
2019-03-16 19:16:11 +0800

    #

---
+++ 使用过的js函数.js
@@ -0,0 +1,40 @@
+//函数
+document.querySelector();
+
+selector.textContent();
+
+alert();
+
+name = prompt();
+
+x = multiply(4, 5);
+
+setAttribute('src', 'http://www.v-in.net');
+getAttribute('class', 'classname');
+
+localStorage.setItem(variable, 'name');
+localStorage.getItem();
+
+para.addEventListener('click', updateName);
+
+para = doucument.createElement("p");
+document.body.appendChild('para');
+
+//=======事件============
+doucument.addEventListener(DOMContentLoaded, function () { });
+// DOMContentLoaded
+
+
+
+// <script async src="js/script.js"></script>
+  //浏览器遇到 async 脚本时不会阻塞页面渲染，而是直接下载然后运行。适用于独立不互相依赖的脚本.效率高,但易出错.
+
+// <script defer src="js/script.js"></script>
+  //脚本按照页面出现的顺序加载和运行;适用于依赖其他脚本或dom元素的脚本.效率低但稳定.
+
+
+
+
+
+
+
